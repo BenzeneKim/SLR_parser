754f6c9f51f080de9192690f452a4c998d736cd5
Benzene Kim <davidkim020409@gmail.com>
2024-06-07 18:58:08 +0900

    feature: parsing works fine

--- python.py
+++ python.py
@@ -9,7 +9,11 @@ table = pd.DataFrame(table)
 input_string = str(open(sys.argv[1], 'r').readline())
 input_string = input_string.split(' ')
 input_string.append('$')
+
+command = []
+
 print(input_string)
+
 # input_string = list(map(str, input_string.split(' ')))
 
 def func_length(s):
@@ -38,6 +42,7 @@ if __name__ == "__main__":
             print(r[int(action[1:])])
             length  = func_length(r[int(action[1:])][1])
             print(handle)
+            command.append(int(action[1:]))
             for _ in range(length):
                 print(input_string)
                 current_state = stack.pop()
@@ -47,10 +52,59 @@ if __name__ == "__main__":
             print(input_string)
             handle -= length
             stack.append(int(table[r[int(action[1:])][0]][current_state]))
+            handle += 1
             print(stack)
         elif action == 'acc':
             print("Success!")
-            exit()
+            command.reverse()
+            print(command)
+            break
+    output = [['CODE',[0]]]
+    layer = 1
+    index = 0
+    i = 0
+    while True:
+        if len(command) == i:
+            break
+        print(index)
+        print(r[command[i]][0])
+        if output[index][0] == r[command[i]][0] and len(output[index][1]) == layer:
+            index2 = 0
+            for j in r[command[i]][1].split(' '):
+                temp = [j, []]
+                for k in  output[index][1]:
+                    temp[1].append(k)
+                temp[1].append(index2)
+                index2+=1
+                output.append(temp)
+            layer += 1
+            i += 1
+            print(output)
+            index = len(output)-1
+            print(index)
+            print(layer)
         else:
-            stack.append(int(current_state))
-            handle += 1
+            index -= 1
+            if index == -1:
+                layer -= 1
+                index = len(output)-1
+    print(output)
+
+# import os
+
+# def print_tree(startpath, indent=''):
+#     items = sorted(os.listdir(startpath))
+#     for i, item in enumerate(items):
+#         path = os.path.join(startpath, item)
+#         is_last = i == len(items) - 1
+#         if os.path.isdir(path):
+#             print(f"{indent}{'└── ' if is_last else '├── '}{item}/")
+#             new_indent = indent + ('    ' if is_last else '│   ')
+#             print_tree(path, new_indent)
+#         else:
+#             print(f"{indent}{'└── ' if is_last else '├── '}{item}")
+
+# if __name__ == '__main__':
+#     startpath = '.'  # Replace with the desired starting directory path
+#     print(startpath)
+#     print_tree(startpath)
